18d4ae9c337125c260892897e7495d7358cc752e
Arch <111789310+Arch881010@users.noreply.github.com>
2023-04-12 13:30:35 -0500

    Feat: Formtting. New: Get

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
-exports.Cache = function(){
-  if(typeof cache == 'undefined') {
-  global.cache = new Map();
+exports.Cache = function() {
+  if (typeof cache == 'undefined') {
+    global.cache = new Map();
   } else {
-    throw new ReferenceError("Cache(); has already been ran before.", "index.js",3);
+    throw new ReferenceError("Cache(); has already been ran before.", "index.js", 3);
   }
 }
 
 
-exports.newCache = function(){
-  if(typeof cache == 'undefined') {
-  global.cache = new Map();
+exports.newCache = function() {
+  if (typeof cache == 'undefined') {
+    global.cache = new Map();
   } else {
-    throw new ReferenceError("newCache(); has already been ran before.", "index.js",3);
+    throw new ReferenceError("newCache(); has already been ran before.", "index.js", 3);
   }
 }
 
 
 //Creates/adds a new item.
 exports.addCache = function(key, value) {
-  if (typeof cache === undefined) {
+  if (typeof cache == 'undefined') {
     throw new Error("Missing cache. (Require and run \"new Cache();\" once and anywhere.)", "index.js", 5);
   }
   key = key ?? null;
   value = value ?? null;
-  if(key === null && value === null) {
+  if (key === null && value === null) {
     throw new ReferenceError("You did not supply a key or a value.");
-  } else if(key === key && value == null) {
+  } else if (key === key && value == null) {
     throw new ReferenceError("You did not supply a value to set it to. Key provided: " + key);
-  } else if(key === null && value == value) {
+  } else if (key === null && value == value) {
     throw new ReferenceError("Key is undefined. Was gave no key and received a value as " + value);
   } else {
     cache.set(key, value);
@@ -37,16 +37,16 @@ exports.addCache = function(key, value) {
 
 
 exports.add = function(key, value) {
-  if (typeof cache === undefined) {
+  if (typeof cache == 'undefined') {
     throw new Error("Missing cache. (Rqure and run \"new Cache();\" once and anywhere.)", "index.js", 5);
   }
   key = key ?? null;
   value = value ?? null;
-  if(key === null && value === null) {
+  if (key === null && value === null) {
     throw new ReferenceError("You did not supply a key or a value.");
-  } else if(key === key && value == null) {
+  } else if (key === key && value == null) {
     throw new ReferenceError("You did not supply a value to set it to. Key provided: " + key);
-  } else if(key === null && value == value) {
+  } else if (key === null && value == value) {
     throw new ReferenceError("Key is undefined. Was gave no key and received a value as " + value);
   } else {
     cache.set(key, value);
@@ -56,6 +56,7 @@ exports.add = function(key, value) {
 
 //Updates a value for an object lazily.
 exports.updateCache = function(key, value) {
+
   addCache(key, value);
 };
 exports.update = function(key, value) {
@@ -65,23 +66,23 @@ exports.update = function(key, value) {
 
 //Deletes a cached item.
 exports.deleteCache = function(key) {
-  if (typeof cache === undefined) {
+  if (typeof cache == 'undefined') {
     throw new Error("Missing cache. (Import and run \"new Cache();\" once and anywhere.)", "index.js", 5);
   }
   key = key ?? null;
-  if(key === null) {
-    throw new ReferenceError("There is no key as \"" + key +"\" in the cache." ) 
+  if (key === null) {
+    throw new ReferenceError("There is no key as \"" + key + "\" in the cache.")
   } else {
     cache.delete(key);
   }
 };
 exports.delete = function(key) {
-  if (typeof cache === undefined) {
+  if (typeof cache == 'undefined') {
     throw new Error("Missing cache. (Import and run \"new Cache();\" once and anywhere.)", "index.js", 5);
   }
   key = key ?? null;
-  if(key === null) {
-    throw new ReferenceError("There is no key as \"" + key +"\" in the cache." );
+  if (key === null) {
+    throw new ReferenceError("There is no key as \"" + key + "\" in the cache.");
   } else {
     cache.delete(key);
   }
@@ -90,7 +91,7 @@ exports.delete = function(key) {
 
 //Global manger
 exports.manageCache = function(action, key, value) {
-  if (cache === undefined) {
+  if (typeof cache == 'undefined') {
     throw new Error("Missing cache. (Import and run newCache(); once and anywhere.)", "index.js", 5);
   };
   const data = require('./names.json');
@@ -98,24 +99,24 @@ exports.manageCache = function(action, key, value) {
   const updateList = data.update;
   const deleteList = data.delete;
   action = action.toLowerCase();
-    if(getList.includes(action)) {
-      if(cache.has(key)) {
-          return cache.get(key);
-      } else {
-        throw new URIError("Invalid key.","index.js",12)
-      }
-    } else if (updateList.includes(action)) {
-      cache.set(key, value);
-      return;
-    } else if (deleteList.includes(action)) {
-      cache.delete(key);
-      return;
+  if (getList.includes(action)) {
+    if (cache.has(key)) {
+      return cache.get(key);
     } else {
-      throw new TypeError("Improper input. Err(Wrong input). [Cache-Info] Try using " + `"new", "update", "edit", "remove", or (more to come)."`, "index.js", 10);
-    };
+      throw new URIError("Invalid key.", "index.js", 12)
+    }
+  } else if (updateList.includes(action)) {
+    cache.set(key, value);
+    return;
+  } else if (deleteList.includes(action)) {
+    cache.delete(key);
+    return;
+  } else {
+    throw new TypeError("Improper input. Err(Wrong input). [Cache-Info] Try using " + `"new", "update", "edit", "remove", or (more to come)."`, "index.js", 10);
+  };
 };
 exports.manage = function(action, key, value) {
-  if (cache === undefined) {
+  if (typeof cache == 'undefined') {
     throw new Error("Missing cache. (Import and run newCache once and anywhere.)", "index.js", 5);
   };
   const data = require('./names.json');
@@ -123,19 +124,46 @@ exports.manage = function(action, key, value) {
   const updateList = data.update;
   const deleteList = data.delete;
   action = action.toLowerCase();
-    if(getList.includes(action)) {
-      if(cache.has(key)) {
-          return cache.get(key);
-      } else {
-        throw new URIError("Invalid key.","index.js",12)
-      }
-    } else if (updateList.includes(action)) {
-      cache.set(key, value);
-      return;
-    } else if (deleteList.includes(action)) {
-      cache.delete(key);
-      return;
+  if (getList.includes(action)) {
+    if (cache.has(key)) {
+      return cache.get(key);
     } else {
-      throw new TypeError("Improper input. Err(Wrong input). [Cache-Info] Try using " + `"new", "update", "edit", "remove", or (more to come)."`, "index.js", 10);
-    };
+      throw new URIError("Invalid key.", "index.js", 12)
+    }
+  } else if (updateList.includes(action)) {
+    cache.set(key, value);
+    return;
+  } else if (deleteList.includes(action)) {
+    cache.delete(key);
+    return;
+  } else {
+    throw new TypeError("Improper input. Err(Wrong input). [Cache-Info] Try using " + `"new", "update", "edit", "remove", or (more to come)."`, "index.js", 10);
+  };
 };
+
+exports.getCache = function(key) {
+  if (typeof cache == 'undefined') {
+    throw new ReferenceError("Cache is undefined, require/import newCache(); and run it.")
+  }
+  if (typeof key == 'undefined') {
+    throw new ReferenceError("You did not send a key to be fetched.")
+  } else if (!cache.has(key)) {
+    return undefined;
+  } else {
+    return cache.get(key);
+  }
+};
+
+exports.get = function(key) {
+  if (typeof cache == 'undefined') {
+    throw new ReferenceError("Cache is undefined, require/import newCache(); and run it.")
+  };
+  if (typeof key == 'undefined') {
+    throw new ReferenceError("You did not send a key to be fetch.")
+  } else if (!cache.has(key)) {
+    return undefined;
+  } else {
+    return cache.get(key);
+  }
+};
+
