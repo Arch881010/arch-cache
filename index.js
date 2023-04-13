8df822a5db750aa823ad765287e5ed5fdd25f807
Arch <111789310+Arch881010@users.noreply.github.com>
2023-04-13 19:32:13 +0000

    Feat: Rewrite, complete overhall.

--- index.js
+++ index.js
@@ -1,26 +1,39 @@
-exports.Cache = function() {
-  if (typeof cache == 'undefined') {
-    global.cache = new Map();
+/*
+JSON.parse(fs.readFileSync("./storageFiles/cache.json","UTF-8"));
+fs.writeFileSync("./storageFiles/cache.json",JSON.stringify(data));
+*/
+exports.new = function(password) {
+  var fs = require('node:fs');
+  var cache = fs.readFileSync("./storageFiles/cache.json","UTF-8");
+  password = password ?? "";
+  obj = {
+    "password":password
+  }
+  if (cache == '{}' || cache == "") {
+    fs.writeFileSync("./storageFiles/cache.json",JSON.stringify(obj));
   } else {
-    throw new ReferenceError("Cache(); has already been ran before.", "index.js", 3);
   }
 }
 
 
-exports.newCache = function() {
-  if (typeof cache == 'undefined') {
-    global.cache = new Map();
+exports.newCache = function(password) {
+  var fs = require('node:fs');
+  var cache = fs.readFileSync("./storageFiles/cache.json","UTF-8");
+  password = password ?? "";
+  obj = {
+    "password":password
+  }
+  if (cache == '{}' || cache == "") {
+    fs.writeFileSync("./storageFiles/cache.json",JSON.stringify(obj));
   } else {
-    throw new ReferenceError("newCache(); has already been ran before.", "index.js", 3);
   }
 }
 
 
 //Creates/adds a new item.
 exports.addCache = function(key, value) {
-  if (typeof cache == 'undefined') {
-    throw new Error("Missing cache. (Require and run \"new Cache();\" once and anywhere.)", "index.js", 5);
-  }
+  var fs = require('node:fs');
+  var data = JSON.parse(fs.readFileSync("./storageFiles/cache.json","UTF-8"));
   key = key ?? null;
   value = value ?? null;
   if (key === null && value === null) {
@@ -30,16 +43,16 @@ exports.addCache = function(key, value) {
   } else if (key === null && value == value) {
     throw new ReferenceError("Key is undefined. Was gave no key and received a value as " + value);
   } else {
-    cache.set(key, value);
+    data[`${key}`] = value;
+    fs.writeFileSync("./storageFiles/cache.json",JSON.stringify(data));
   }
 };
 
 
 
 exports.add = function(key, value) {
-  if (typeof cache == 'undefined') {
-    throw new Error("Missing cache. (Rqure and run \"new Cache();\" once and anywhere.)", "index.js", 5);
-  }
+  var fs = require('node:fs');
+  var data = JSON.parse(fs.readFileSync("./storageFiles/cache.json","UTF-8"));
   key = key ?? null;
   value = value ?? null;
   if (key === null && value === null) {
@@ -49,121 +62,110 @@ exports.add = function(key, value) {
   } else if (key === null && value == value) {
     throw new ReferenceError("Key is undefined. Was gave no key and received a value as " + value);
   } else {
-    cache.set(key, value);
+    data[`${key}`] = value;
+    fs.writeFileSync("./storageFiles/cache.json",JSON.stringify(data));
   }
 };
 
 
-//Updates a value for an object lazily.
+//Updates a value for an key.
 exports.updateCache = function(key, value) {
-
-  addCache(key, value);
+  var fs = require('node:fs');
+  var data = JSON.parse(fs.readFileSync("./storageFiles/cache.json","UTF-8"));
+  key = key ?? null;
+  value = value ?? null;
+  if (key === null && value === null) {
+    throw new ReferenceError("You did not supply a key or a value.");
+  } else if (key === key && value == null) {
+    throw new ReferenceError("You did not supply a value to set it to. Key provided: " + key);
+  } else if (key === null && value == value) {
+    throw new ReferenceError("Key is undefined. Was gave no key and received a value as " + value);
+  } else {
+    data[`${key}`] = value;
+    fs.writeFileSync("./storageFiles/cache.json",JSON.stringify(data));
+  }
 };
 exports.update = function(key, value) {
-  addCache(key, value);
+  var fs = require('node:fs');
+  var data = JSON.parse(fs.readFileSync("./storageFiles/cache.json","UTF-8"));
+  key = key ?? null;
+  value = value ?? null;
+  if (key === null && value === null) {
+    throw new ReferenceError("You did not supply a key or a value.");
+  } else if (key === key && value == null) {
+    throw new ReferenceError("You did not supply a value to set it to. Key provided: " + key);
+  } else if (key === null && value == value) {
+    throw new ReferenceError("Key is undefined. Was gave no key and received a value as " + value);
+  } else {
+    data[`${key}`] = value;
+    fs.writeFileSync("./storageFiles/cache.json",JSON.stringify(data));
+  }
 }
 
 
 //Deletes a cached item.
 exports.deleteCache = function(key) {
-  if (typeof cache == 'undefined') {
-    throw new Error("Missing cache. (Import and run \"new Cache();\" once and anywhere.)", "index.js", 5);
-  }
+  var fs = require('node:fs');
+  var data = JSON.parse(fs.readFileSync("./storageFiles/cache.json","UTF-8"))
   key = key ?? null;
-  if (key === null) {
-    throw new ReferenceError("There is no key as \"" + key + "\" in the cache.")
+  if (key === null || typeof data[`${key}`] == 'undefined' ) {
+    throw new ReferenceError("There is no key as \"" + key + "\" in the cache.");
   } else {
-    cache.delete(key);
+    delete data[`${key}`];
+    fs.writeFileSync("./storageFiles/cache.json",JSON.stringify(data));
   }
 };
 exports.delete = function(key) {
-  if (typeof cache == 'undefined') {
-    throw new Error("Missing cache. (Import and run \"new Cache();\" once and anywhere.)", "index.js", 5);
-  }
+  var fs = require('node:fs');
+  var data = JSON.parse(fs.readFileSync("./storageFiles/cache.json","UTF-8"))
   key = key ?? null;
-  if (key === null) {
+  if (key === null || typeof data[`${key}`] == 'undefined' ) {
     throw new ReferenceError("There is no key as \"" + key + "\" in the cache.");
   } else {
-    cache.delete(key);
+    delete data[`${key}`];
+    fs.writeFileSync("./storageFiles/cache.json",JSON.stringify(data));
   }
 };
 
-
-//Global manger
-exports.manageCache = function(action, key, value) {
-  if (typeof cache == 'undefined') {
-    throw new Error("Missing cache. (Import and run newCache(); once and anywhere.)", "index.js", 5);
-  };
-  const data = require('./names.json');
-  const getList = data.get;
-  const updateList = data.update;
-  const deleteList = data.delete;
-  action = action.toLowerCase();
-  if (getList.includes(action)) {
-    if (cache.has(key)) {
-      return cache.get(key);
-    } else {
-      throw new URIError("Invalid key.", "index.js", 12)
-    }
-  } else if (updateList.includes(action)) {
-    cache.set(key, value);
-    return;
-  } else if (deleteList.includes(action)) {
-    cache.delete(key);
-    return;
-  } else {
-    throw new TypeError("Improper input. Err(Wrong input). [Cache-Info] Try using " + `"new", "update", "edit", "remove", or (more to come)."`, "index.js", 10);
-  };
-};
-exports.manage = function(action, key, value) {
-  if (typeof cache == 'undefined') {
-    throw new Error("Missing cache. (Import and run newCache once and anywhere.)", "index.js", 5);
-  };
-  const data = require('./names.json');
-  const getList = data.get;
-  const updateList = data.update;
-  const deleteList = data.delete;
-  action = action.toLowerCase();
-  if (getList.includes(action)) {
-    if (cache.has(key)) {
-      return cache.get(key);
-    } else {
-      throw new URIError("Invalid key.", "index.js", 12)
-    }
-  } else if (updateList.includes(action)) {
-    cache.set(key, value);
-    return;
-  } else if (deleteList.includes(action)) {
-    cache.delete(key);
-    return;
-  } else {
-    throw new TypeError("Improper input. Err(Wrong input). [Cache-Info] Try using " + `"new", "update", "edit", "remove", or (more to come)."`, "index.js", 10);
-  };
-};
 
 exports.getCache = function(key) {
-  if (typeof cache == 'undefined') {
-    throw new ReferenceError("Cache is undefined, require/import newCache(); and run it.")
-  }
-  if (typeof key == 'undefined') {
-    throw new ReferenceError("You did not send a key to be fetched.")
-  } else if (!cache.has(key)) {
-    return undefined;
+  var fs = require('node:fs');
+  var data = JSON.parse(fs.readFileSync("./storageFiles/cache.json","UTF-8"));
+  if(typeof data.key == 'undefined' ) {
+    throw new ReferenceError(`There is no key as ${key} in the Cache.`);
   } else {
-    return cache.get(key);
+    return data.key;
   }
 };
 
 exports.get = function(key) {
-  if (typeof cache == 'undefined') {
-    throw new ReferenceError("Cache is undefined, require/import newCache(); and run it.")
-  };
-  if (typeof key == 'undefined') {
-    throw new ReferenceError("You did not send a key to be fetch.")
-  } else if (!cache.has(key)) {
-    return undefined;
+  var fs = require('node:fs');
+  var data = JSON.parse(fs.readFileSync("./storageFiles/cache.json","UTF-8"));
+  if(typeof data.key == 'undefined' ) {
+    throw new ReferenceError(`There is no key as ${key} in the Cache.`);
   } else {
-    return cache.get(key);
+    return data.key;
   }
 };
 
+exports.clear = function(password){
+  password = password ?? "";
+  var fs = require('node:fs');
+  var cachePassword = JSON.parse(fs.readFileSync("./storageFiles/cache.json","UTF-8")).password ?? "";
+  if (password != cachePassword) {
+    console.log("Improper password.");
+    return;
+  };
+  fs.writeFileSync("./storageFiles/cache.json", "{}");
+}
+
+exports.clearCache = function(password){
+  password = password ?? "";
+  var fs = require('node:fs');
+  var cachePassword = JSON.parse(fs.readFileSync("./storageFiles/cache.json","UTF-8")).password ?? "";
+  if (password != cachePassword) {
+    console.log("Improper password.");
+    return;
+  };
+  fs.writeFileSync("./storageFiles/cache.json", "{}");
+}
